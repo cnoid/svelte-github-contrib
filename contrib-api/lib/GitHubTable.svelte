424ef86fbab30f6d005c28b3e40157994699bf04
Eivind <10182309+cnoid@users.noreply.github.com>
2023-11-17 23:44:56 +0100

    Update GitHubTable.svelte
    CSS rework to adjust for API so it prints like Github's profile

--- contrib-api/lib/GitHubTable.svelte
+++ contrib-api/lib/GitHubTable.svelte
@@ -12,7 +12,7 @@
     margin: 0;
     padding: 0;
   }
-
+  
   .github-contributions {
     color: #c9d1d9;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
@@ -25,57 +25,58 @@
   }
 
   .contributions-header {
-    position: absolute;
-    top: 0;
-    left: 0;
-    transform: translateY(-100%);
-    font-size: 14px;
-    font-weight: 600;
-  }
+  position: absolute;
+  top: -20px; 
+  left: 0; 
+  font-size: 12px;
+  font-weight: 600;
+}
 
   .contributions-container {
-    border: 1px solid #fff;
-    border-radius: 6px;
+    border: 0.5px solid #fff;
     padding: 8px;
-    background-color: #0d1117;
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    align-items: start;
-    grid-gap: 2px;
-  }
-
-  .day-labels {
-    grid-column: 1;
-    font-size: 9px;
-    color: #8b949e;
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding-right: 8px;
-    height: calc(7 * 12px);
+    gap: 4px;
+    align-items: flex-start;
+    padding-left: 20px; 
+    background-color: #0d1117;
   }
 
-  .contributions-grid {
-    grid-column: 2;
-    display: grid;
-    grid-template-columns: repeat(53, 12px);
-    grid-template-rows: repeat(7, 12px);
-    gap: 2px;
-  }
+  .week-container {
+  display: grid;
+  grid-template-rows: repeat(7, 1fr);
+  border: none;
+  border-radius: 0; 
+  overflow: hidden;
+}
 
   .contribution-cell {
-    width: 10px;
-    height: 10px;
-    border-radius: 2px;
-    background-color: #161b22;
-  }
+  color: #8b949e;
+  width: 10px;
+  height: 10px; 
+  border-radius: 2px;
+  background-color: #161b22;
+  margin-bottom: 1px;
+  margin-top: 1px;
+}
 
   .contribution-cell.level-1 { background-color: #0e4429; }
   .contribution-cell.level-2 { background-color: #006d32; }
   .contribution-cell.level-3 { background-color: #26a641; }
   .contribution-cell.level-4 { background-color: #39d353; }
 
-  .legend {
+  .day-labels {
+  
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  font-size: 12px;
+  margin-top: 3px;
+  margin-right: 3px;
+  order: -1; 
+}
+
+.legend {
     position: absolute;
     bottom: 0;
     right: 0;
@@ -88,9 +89,9 @@
   }
 
   .legend-item {
-  display: flex;
-  align-items: center;
-  margin-bottom: 4px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 4px;
   }
 
   .legend-color {
@@ -107,6 +108,7 @@
     display: flex;
     align-items: center;
   }
+
   .legend-color.level-4 { background-color: #39d353; }
 </style>
 
